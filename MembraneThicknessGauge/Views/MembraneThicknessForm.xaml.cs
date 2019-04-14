1c85bd8ae3b949af7a326796fd4f02f74cc8c660
Montmorency <sascha.chernyx@gmail.com>
2019-04-14 15:19:10 +0700

    14 apr 2019

---
+++ MembraneThicknessGauge/Views/MembraneThicknessForm.xaml.cs
@@ -0,0 +1,33 @@
+﻿using MembraneThicknessGauge.Model;
+using MembraneThicknessGauge.ViewModels;
+using System;
+using System.Windows;
+
+namespace MembraneThicknessGauge
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ConnectionClient connectionClient = new ConnectionClient();
+
+        public MainWindow()
+        {
+            var viewModel = new MainViewModel();
+            viewModel.IPString = "numb";
+
+            DataContext = viewModel;
+            InitializeComponent();
+
+            viewModel.IPString = "172.22.11.2";
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+    }
+}
---
+++ MembraneThicknessGauge/Views/SpeedTestView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using MembraneThicknessGauge.Utilites;
+using System;
+using System.Windows.Controls;
+
+namespace MembraneThicknessGauge.Views
+{
+    /// <summary>
+    /// Interaction logic for SpeedTestView.xaml
+    /// </summary>
+    public partial class SpeedTestView : UserControl
+    {
+        public SpeedTestView()
+        {
+            InitializeComponent();
+        }
+    }
+}
